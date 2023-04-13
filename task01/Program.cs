4697648b7a5f6cde33de24d6b7a12e1d4d80df09
ваше_имя <grigorenko-marko@rambler.ru>
2023-04-13 20:37:08 +0300

    task01 пример от Киррила

---
+++ task01/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Array:");
+int[] array = ReadArray();
+
+Console.WriteLine(string.Join(';', array));
+
+
+
+int[] ReadArray()
+{
+	string arrayAsString = Console.ReadLine(); // 1, 2, 3, 4
+	string[] numbersAsString = arrayAsString.Split(", "); // Разбить строку на массив строк учитывая разделитель
+
+	int[] array = new int[numbersAsString.Length];
+
+	for (int i = 0; i < array.Length; i++)
+	{
+		array[i] = int.Parse(numbersAsString[i]);
+	}
+
+	return array;
+}
